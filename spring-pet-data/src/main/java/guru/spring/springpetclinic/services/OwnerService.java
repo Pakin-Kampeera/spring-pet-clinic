7786f659d5f3439519cf41cc15355f16681bdd21
pakin-kampeera <armpakin@hotmail.com>
2021-10-23 15:11:06 +0700

    Add: Banner and Optimize Interface

--- spring-pet-data/src/main/java/guru/spring/springpetclinic/services/OwnerService.java
+++ spring-pet-data/src/main/java/guru/spring/springpetclinic/services/OwnerService.java
@@ -4,9 +4,6 @@ import guru.spring.springpetclinic.models.Owner;
 
 import java.util.Set;
 
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName(String name);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
 }
